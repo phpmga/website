92cd1041e21a0d7cf73161cb5d1a1ffe7595dd8f
Vanderson Lopes <vandersonlopes@yahoo.com.br>
2015-08-29 15:04:56 -0300

    added Google Analytics tracking code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,18 @@
 </head>
 <body>
 
+<!-- Google Analytics -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-66920404-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
 <!-- Following Menu -->
 <div class="ui large top fixed hidden menu phpmga-menu-hidden">
     <div class="ui container">
