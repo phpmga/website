894e0caf51fe828881f821d9c9d19be46d20d0c3
avlima <albertovieiradelima@gmail.com>
2015-09-19 16:12:08 -0300

    iclude meta tag youtube page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="DbMCcMUJ8xPX65DfsCnDxlJBJEDI-xnPRAGCNqyGCe8"/>
     <meta name="description" content="Web Site da Comunidade PHP Maringá">
     <meta name="author" content="Comunidade PHP Maringá">
 
